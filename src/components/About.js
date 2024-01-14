3272fbac874ae2af511deb20bdd44776e54f17d4
Kylie Clark <kyliedarlene@icloud.com>
2024-01-14 14:19:16 -0700

    About <img> complete

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About({ 
+    image = "https://via.placeholder.com/215"
+}) {
+    return (
+        <aside>
+            <img src={image} alt="blog logo"/>
+        </aside>
+    )
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import About from "./About";
 import blogData from "../data/blog";
-
-console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      <Header name={blogData.name}/>
+      <Header name={blogData.name} />
+      <About image={blogData.image} />
     </div>
   );
 }
