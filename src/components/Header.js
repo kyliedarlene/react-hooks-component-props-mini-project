d4974f5cde6e890de4a241fad9da64e3c5bfa8ab
Kylie Clark <kyliedarlene@icloud.com>
2024-01-14 14:39:06 -0700

    added <header> to Header function to pass test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header({ name }) {
-    return <h1>{name}</h1>
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+    )
 }
 
 export default Header;
